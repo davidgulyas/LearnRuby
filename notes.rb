8ac083b0ea2422ca6e7f6ff06aed214954014b87
davidgulyas <93352151+davidgulyas@users.noreply.github.com>
2021-12-08 13:08:29 -0700

    Finish notes to end of Ch 3

--- notes.rb
+++ notes.rb
@@ -1,5 +1,6 @@
-
-name = gets()
+print "Enter a name: "
+#name = gets()
+name = "h"
 puts( "Hello #{name}" )  #string interpolation, runs eval on contents
 # only works for double quote string, not single
 
@@ -21,13 +22,25 @@ if subtotal < 0.0
     subtotal = 0.0
 end # end is required
 
-exampleVar = 1 #local variables begin with lowercase
+#Scope / variable types
+exampleVar = 1 #local variables begin with lowercase or _
+                #Scoped to the construct in which they're declared
 $exampleVar = 1 #global variables start with $
+                #Scoped to anywhere in the program.
+                #There are several predefined ones that hold information about the runtime
+                #$_ holds the last string read by gets
+@exampleVar = 1 #instance variables start with @, unique to each instance
+@@exampleVer = 1 #Class variables are shared between all instances of a class. (Static)
+EXAMPLECONST = 1 #constants begin with capitals. Shouldn't be changed.
+                #scoped to the class or module in which they're declared.
+                #if declared outside class or module, have global scope
+                #Class names mut be constants
+
 
 # in ruby everything is an object even ints
 puts 5.to_s + 'a'
 
-class Dog
+class Dog #Must start with capital. Makes it a Constant
     def set_name( name )
         @dogName = name #instance variables start with @
     end
@@ -41,8 +54,6 @@ class Dog
     end
 end
 
-#Question: what happens if you create a local variable in a class?
-#It would just die when then scope ends?
 
 #initializer is .new
 dog1 = Dog.new
@@ -50,6 +61,7 @@ dog1.set_name('jonDog')
 puts dog1.get_name
 
 #if an instance variable is undefined, 'nil' is returned.
+print "nil:"
 puts dog1.returnUndefined
 
 # Ruby puts the initial script being run in a 'main' class instance
@@ -78,7 +90,7 @@ end
 
 #super function calls a method with the same name as the current method in the base class.
 #useful for calling the base constructor.
-#'super()' passes no arguments. 'super' passess all arguments
+#'super()' passes no arguments. 'super' passes all arguments
 
 # = at the end of the function name (no spaces) is syntactic sugar to create a setter of a classes value
 def thing=(aThing)
@@ -95,6 +107,7 @@ class SetGet
 end
 
 #A symbol is a name preceded by a :. Represents names inside the interpreter
+#What all can you do with symbols?
 
 #attr_* can take lists of symbols
 #attr_reader(:name, :desc)
@@ -103,7 +116,120 @@ end
 
 #Standard way of finding the method in the inheritance chain. Go upwards until a match is found.
 
-#'class variables' start with @@
-#They're shared by all instances, similar to static variables in C#
-
 #.super returns a class's superclass
+
+# :: is the scopre resolution operator.
+# from https://stackoverflow.com/a/5033013
+module Music
+    module Record
+      # perhaps a copy of Abbey Road by The Beatles?
+    end
+end
+  
+module Record
+    # for adding an item to the database
+end
+
+::Record #gets the Record at the "top level" of scope
+Music::Record #gets the Record inside the Music module
+
+
+#classes can be extended/patched by redeclaring them.
+#The first declaration sets the base class. (No inheritance actually inherits from Object class) The other declarations can set the same base class or set nothing
+class Array
+    def gribbet
+        puts "I'm a new thing"
+    end
+end
+
+[1].gribbet
+
+class J < Array
+    @i = 1
+end
+
+#class J < String #Not allowed to change base class
+
+class J #Allowed to not respecify base class
+    @ii = 2
+end
+
+#Chapter 3 Strings & Ranges
+
+'It\'s here' # \ escapes '
+"#{1+2} \t \n" #"" strings can do evaluation
+
+'\t \n' #doesn't work. '' doesn't evluate special characters like that
+
+
+# You can use custom string deliminators
+%Q/This is a "" string/
+%/This is a "" string/
+%q/This is a '' string/
+
+# The deliminator can be arbitrary. Probably don't do this?
+%Q[This is a "" string]
+
+#strings deliminated by `` or %x// are exaluated by the runtime console (passed to te oeprating system)
+puts(`ls`)
+puts()
+puts(%x{ls})
+
+#embed commands in other strings!!
+#print("Goodbye #{%x{open -a Calculator}}")
+
+# Concat strings in 3 ways
+"Hi " << "x"
+"Hi " + "x"
+"Hi " "x"
+
+# Ruby had imutable strings in 2.x, but now defaults to immutable strings in 3.x
+a = "test"
+p(a.object_id)
+a.capitalize! #now holds Test #wtf
+p(a.object_id)
+#a has been changed, but the instance is the same.
+
+#There's no notion of a char?
+a[1] = 'x' #sets the second character to x
+p = a[1] #sets p to the ascii value of the second character. #wtf
+p = a[1,1] #sets p to the 'char' value of the second character. Specify start index and lenght
+p = a[1,3] #sets p to the second-forth substring of a.
+p = a[1..4] #does the same things as above. Specify start and end index
+p = a[-1, 1] #sets p to last character of a
+p = a[-5..-1] #sets p to the last 5 characters of a
+
+"test\r\n".chomp #removes the newline characters. chop does something similar but is bullshit
+#You can mess around with what the "record seperator" is, but that seems bad.
+
+printf("%1.2f", 10.1283948) #printf exists. Can't find doc on the 1.2 specifier.
+puts()
+
+a = ('a'..'d') #ranges can take string args.
+puts a.to_a #convert range to an array
+('a'...'d') # triple . omits the last element. Useful for looping? (0...a.length) maybe?
+('abc' ..'def') #has 2110 elements
+
+for i in (1..3) do #range in for loop
+    puts(i)
+end
+
+#heredocs are a thing. They let you create a literal that saves formatting and newlines.
+#Give it an 'end marker' and it'll stoore everything until it see it again.
+#The magic is the  << character, or <<- if you want to indent the end marker
+hdocvar = <<EODOCMARKER
+Cool
+    Formatting
+EODOCMARKER
+puts(hdocvar)
+
+#Literals
+#start with % and then a letter specifying which type of literal.
+# %s/dog/   #specifies the :dog symbol
+#Q: what is the puropse of this since you can just go :dog? I guess evaluating is useful for arrat literals? But can do it with .add?
+puts( %w/4 #{1+2}/ ) #creates a list without evaluating
+puts( %W/4 #{1+2}/ ) #creates a list with exaluating.
+
+#Charpter 4 Arrays and Hashes
+
+ 
